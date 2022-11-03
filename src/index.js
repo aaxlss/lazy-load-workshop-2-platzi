27798efc8abe7fac01cd3bf60849184c0d497492
aaxlss <axl9305@gmail.com>
2022-11-03 13:24:29 +0000

    [Update] Updating index html. Deleting extra iteams and adding new main div to insert images. [Update] Adding js code to create button and add image to the main div

--- src/index.js
+++ src/index.js
@@ -4,3 +4,35 @@
  **/
 
 console.log('Happy hacking :)')
+//URL info
+const baseURL = 'https://randomfox.ca/'
+const apiURL = () => {
+    let min = Math.ceil(1);
+    let max = Math.floor(123);
+    let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    return `${baseURL}images/${randomNumber}.jpg`}
+//main div
+const images = document.querySelector('#images');
+
+//add btn
+const btnAddImg = document.createElement('button');
+btnAddImg.textContent = 'Add image';
+
+const addImageFn = () => {
+    window.fetch(apiURL())
+    .then(({url}) =>{
+        const divImg = document.createElement('div');
+        const img = document.createElement('img');
+        
+        divImg.className = 'p-4';
+        img.className = 'mx-auto';
+        img.width = 320;
+        img.src = url
+
+        divImg.append(img);
+        images.append(divImg)
+    });
+}
+btnAddImg.addEventListener('click', addImageFn);
+images.append(btnAddImg)
+//image elements
